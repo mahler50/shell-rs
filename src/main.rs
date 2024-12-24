269a085799ce556ecd19cff761903e37b826b322
whn <1874695345@qq.com>
2024-12-24 13:06:42 +0800

    run a program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
+use std::process::{self, Command};
 
+// split command with whitespace
 fn tokenize(s: &str) -> Vec<&str> {
     s.split(" ").collect()
 }
@@ -23,6 +24,16 @@ fn type_builtin(args: Vec<&str>, path: String) {
     });
 }
 
+fn find_executable_file(file_name: &str , path: String) -> Option<String> {
+    let split = &mut path.split(":");
+    if let Some(path) = 
+        split.find(|path| std::fs::metadata(format!("{}/{}", path, file_name)).is_ok()) {
+            return Some(format!("{}/{}", path, file_name));
+    }
+
+    None
+}
+
 fn main() {
     let stdin = io::stdin();
     let path_env = std::env::var("PATH").unwrap();
@@ -39,7 +50,14 @@ fn main() {
             ["echo", ..] => println!("{}", tokens[1..].join(" ")),
             ["type", ..] => type_builtin(tokens[1..].to_vec(), path_env.clone()),
             _ => {
-                println!("{}: command not found", command);
+                if let Some(_) = find_executable_file(tokens[0], path_env.clone()) {
+                    Command::new("path")
+                    .args(&tokens[1..])
+                    .status()
+                    .expect("failed to execute process");
+                } else {
+                    println!("{}: command not found", command);
+                }
             }
         }
         input.clear();
