2dcfc25e50ab40c36030ef587f0552ba700b91a8
whn <1874695345@qq.com>
2024-12-24 09:44:50 +0800

    the echo builtin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
+
+fn tokenize(s: &str) -> Vec<&str> {
+    s.split(" ").collect()
+}
 
 fn main() {
     // Wait for user input
@@ -9,10 +14,13 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
         stdin.read_line(&mut input).unwrap();
-        match input.trim() {
-            "exit 0" => break,
+        let command = input.trim();
+        let tokens = tokenize(command);
+        match tokens[..] {
+            ["exit", code] => process::exit(code.parse::<i32>().unwrap()),
+            ["echo", ..] => println!("{}", tokens[1..].join(" ")),
             _ => {
-                println!("{}: command not found", input.trim());
+                println!("{}: command not found", command);
             }
         }
         input.clear();
